516dabba1bfe0d8a45686b0f5fdad74cd57006cd
Rod512 <rudro149187@bsdi-bd.org>
2023-08-30 15:23:14 +0600

    colors customize done

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary": "#5E3BEE",
+        "backgroundshade": "F5FCFF",
+        "dribble": "#E62872",
+        "dody": "#1C1E53"
+      }
+    },
   },
   plugins: [],
 }
